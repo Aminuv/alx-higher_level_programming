e7745c0ced56b64e68368a0c877060c87bf90699
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-06 09:25:56 -0700

    tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num < 10:
-        print("0{}, " .format(num), end="")
-    elif num < 99:
-        print("{}, " .format(num), end="")
+    if num != 99:
+        print("{:02d}, " .format(num), end="")
     else:
-        print("{}" .format(num))
+        print("{:02d}" .format(num))
 
