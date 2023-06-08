ca3c7454bdd7448640c58180ee5029db2f5b6c16
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-08 10:31:39 -0700

    tasks01

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,6 @@
 if __name__ == '__main__':
     from sys import argv
     num = len(argv) - 1
-
     if num == 0:
         print("{} arguments.".format(num))
     elif num == 1:
@@ -10,7 +9,7 @@ if __name__ == '__main__':
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(num))
-
+        num = 1
         for arg in argv[1:]:
             print("{}: {}".format(num, arg))
             num += 1
