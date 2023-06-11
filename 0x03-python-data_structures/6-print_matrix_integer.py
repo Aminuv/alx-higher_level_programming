9380334dd9c7239c99be35363bf4c25655b37705
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-10 18:39:41 -0700

    tasks06x

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         for i in row:
             if i != row[-1]:
                 print("{:d} ".format(i), end='')
-                i =+ 1
+                i += 1
             else:
-                print("{:d} ".format(i))
+                print("{:d}".format(i))
