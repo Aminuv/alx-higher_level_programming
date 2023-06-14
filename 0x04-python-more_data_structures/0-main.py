01ab91ebdd8edea85a748870cf5e66c1545dbe80
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-14 08:15:51 -0700

    tasks00

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -9,4 +9,5 @@ matrix = [
 
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
-print(matrix
+print(matrix)
+
--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    for num in matrix:
+        matrix.append(list(map(lambda x: x**2, num)))
+        return matrix
