3c604a067ad39f962a0d12ccf78e65615eb89fb3
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-26 19:57:44 -0700

    tasks 103

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,9 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list - basic for about Python lists.
+ * @p: the PyObject for that  list.
+ */
+void print_python_list(PyObject *p)
+{
