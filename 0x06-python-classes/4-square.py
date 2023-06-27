d91f684bf83cdf0570a25476a36cdd00c7cdf090
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-27 11:32:14 -0700

    tasks 04

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""defines a square class"""
+
+
+class Square:
+    """Represent a square..."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """return value to the size function"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the size of value"""
+
+        if not isinstance(valus, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Return the current area of square"""
+        return self.__size * self.__size
