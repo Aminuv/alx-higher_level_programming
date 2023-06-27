ca483da958fe9e4f2cbc1044ebb949ae8cabf46a
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-26 19:47:09 -0700

    tasks 102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    rest = 0
+    for i in range(1, 3)
+    try:
+        if i > a:
+            raise Exception('too far')
+        else:
+            rest += (a ** b) / i
+        except Exception:
+            rest = a + b
+            break
+        return rest
