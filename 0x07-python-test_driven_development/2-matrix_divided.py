b7705fb8fda0ac3ec6e557e01c0918b3a974b4c1
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-05 04:54:59 -0700

    tasks 01

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" The divides all elements of a matrix."""
+
+"""The module is matrix_divided"""
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by a given numbe"""
-    if not isinstance(matrix, list):
-        raise TypeError(
-            'matrix must be a matrix (list of lists) of integers/floats')
+    """Returns a new matrix"""
+    new_matrix = []
+    error = "matrix must be a matrix (list of lists) of integers/floats"
 
-    if not isinstance(div, (int, float)):
+    if not matrix or matrix is [[]] or matrix is None:
+        raise TypeError(error)
+    if type(div) is int or type(div) is float or type(div) is None:
+        pass
+    else:
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError(
-                'matrix must be a matrix (list of lists) of integers/floats')
-        items_in_first_row = len(matrix[0])
-        if len(row) != items_in_first_row:
+    if matrix[0]:
+        length = len(matrix[0])
+    else:
+        raise TypeError(error)
+    for i in range(len(matrix)):
+        if len(matrix[i]) is not length:
             raise TypeError("Each row of the matrix must have the same size")
-        for items in row:
-            if not isinstance(items, (int, float)):
-                raise TypeError(
-                    'matrix must be a matrix (list of lists) of integers/floats')
-
-    return [list(map(lambda item: round((item / div), 2), row)) for row in matrix]
+        new_matrix.append([])
+        for n in matrix[i]:
+            if type(n) is int or type(n) is float:
+                new_matrix[i].append(round(n / div, 2))
+            else:
+                raise TypeError(error)
+    return new_matrix
