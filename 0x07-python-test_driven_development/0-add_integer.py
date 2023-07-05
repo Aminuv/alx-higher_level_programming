46c8a11d136fcb2dc33e354bb1c6d891a620965f
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-05 03:52:48 -0700

    tasks 00

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,11 +3,10 @@
 """modulle add intigers its add tow intigers"""
 
 
-
 def add_integer(a, b=98):
     """the add integer"""
-    if not isinstance(a,(int,float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b,(int,float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a)+int(b))
