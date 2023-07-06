5081e68ea497c878665dea921e2e5eb250e10278
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-06 10:29:48 -0700

    tasks 102

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,4 @@
-#include "Python.h"
+#include <Python.h>
 /**
  * print_python_string - For Printing an  information about Python str.
  * @p: A PyObject string.
@@ -6,7 +6,7 @@
 
 void print_python_string(PyObject *p)
 {
-	long int len;
+	long int length;
 
 	fflush(stdout);
 
@@ -17,12 +17,12 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	len = ((PyASCIIObject *)(p))->len;
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  lenght: %ld\n", len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
