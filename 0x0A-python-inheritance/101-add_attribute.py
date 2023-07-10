af56ef58d151e22e78aed2a305928316dc98cce7
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-10 12:12:36 -0700

    tasks 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" function that adds a new attribute"""
+
+
+def add_attribute(*args):
+    if "main" in str(type(args[0])):
+        setattr(args[0], args[1], args[2])
+    else:
+        raise TypeError("can't add new attribute")
