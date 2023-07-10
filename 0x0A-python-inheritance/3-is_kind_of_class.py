affe60b1360c80801264007cc3d67c9c9b61270a
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-10 11:17:51 -0700

    tasks 02

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
-      """isinstance for comparison"""
-      
-      return isinstance(obj, a_class)
+    """isinstance for comparison"""
+
+    return isinstance(obj, a_class)
