13fac56614ca018b085bc627d3c7daa9e023cbeb
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-11 03:07:46 -0700

    tasks 00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@
 def read_file(filename=""):
     """ print the contents """
 
-    with open(filname, encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
