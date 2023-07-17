a64cb04c730aa31e4a56fa9e54386f8dd255dcda
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-17 16:18:47 -0700

    tasks 01

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
+
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Represent the base model."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base."""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
