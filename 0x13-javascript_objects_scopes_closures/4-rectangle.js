a009489ebadd038aaab175bc624a612885137eef
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-12 17:34:19 -0700

    up datex tasks

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,22 +8,25 @@ class Rectangle {
   }
 
   print () {
-    for (let x = 0; x < this.height; x++) {
-      let r = '';
-      for (let y = 0; y < this.width; y++) {
-        r += 'X';
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
       }
-      console.log(r);
+      console.log(s);
     }
   }
-}
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
 
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
