871098563499f8fe92af333f8845e70cfa9b5744
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-22 15:47:12 -0700

    add tasks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
     The Lists all States and corresponding Cities
     in the database 'hbtn_0e_101_usa'.
@@ -12,19 +11,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # To Create The 'SQLAlchemy'.
+    # To Create 'SQLAlchemy' engine.
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # To Create a session 'factory'
     Session = sessionmaker(bind=engine)
-
-    # To Create new session obj.
     session = Session()
 
-    # The print them along with their 'Cities'.
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
