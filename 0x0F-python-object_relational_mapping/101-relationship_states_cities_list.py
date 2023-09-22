fbfbaa95bb56d55c6d2edd0281d406419424c23d
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-22 16:32:52 -0700

    tasks 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,13 +5,12 @@
 """
 
 import sys
-from relationship_state import State
-from relationship_city import Cityi
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
 if __name__ == "__main__":
-    # To Create the 'MySQL'.
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
