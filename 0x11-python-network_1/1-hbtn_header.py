01a5354ea5a200cf199f7a5c7e5d2a837a517ed3
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-29 10:18:55 -0700

    tasks 00 and 01 and 02 and 03 and 04

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+script that takes in a 'URL', sends a request to 
+the 'URL' and displays the value of the 'X-Request-Id'.
+"""
+import sys
+from urllib import request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with request.urlopen(url) as response:
+        print(dict(response.headers).get("X-Request-Id"))
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+ script that fetches:
+    - https://alx-intranet.hbtn.io/status
+"""
+    import requests
+
+
+if __name__ == '__main__':
+
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
