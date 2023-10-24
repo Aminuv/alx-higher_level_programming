78db5e393cbd4378ceb5bd6ffc77cb85a5e2a9dd
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-24 21:51:20 +0000

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (erorr, data) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
-	console.log(data);
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
