8857875aec154454689135c4329462b1d330fe79
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-24 14:35:17 -0700

    all at once

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const URL = process.argv[2];
+const fPath = process.argv[3];
+
+request(URL, (error, response, body) => {
+	if (!error) {
+		fs.writeFile(fPath, body, 'utf-8', err => {
+			if (err) console.error(err);
+		});
+	}
+});
---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const URL = process.argv[2];
+const dic = {};
+
+request(URL, (error, response, body) => {
+	if (!error) {
+		const todos = JSON.parse(body);
+		for (const task of todos) {
+			if (task.completed && !(task.userId in dic)) {
+				dic[task.userId] = 0;
+			}
+			if (task.completed) {
+				dic[task.userId] += 1;
+			}
+		}
+		console.log(dic);
+	}
+});
