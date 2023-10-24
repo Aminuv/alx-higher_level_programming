9db767a8efa828281e89aefc6d69aeaba8ed7852
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-24 21:54:35 +0000

    Update 2-tasks

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 const URL = process.argv[2];
 request(URL, (error, response, body) => {
-	if (error) console.log(error);
-	console.log('code: ' + response.statusCode);
+  if (error) console.log(error);
+  console.log('code: ' + response.statusCode);
 });
