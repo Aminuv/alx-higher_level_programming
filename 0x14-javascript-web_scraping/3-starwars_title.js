c87d89debd1dc5279793e178d6475d1b0d25f4a7
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-24 21:55:14 +0000

    Update 3-tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const idu = process.argv[2];
 const URL = 'https://swapi-api.alx-tools.com/api/films/' + idu;
 request(URL, (error, response, body) => {
-	if (error) console.log(error);
-	const data = JSON.parse(body);
-	console.log(data.title);
+  if (error) console.log(error);
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
