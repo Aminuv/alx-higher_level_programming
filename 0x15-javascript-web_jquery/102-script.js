43fbf0cd4aac66e7dd4c83c5d6d10d00d6f909eb
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-11-01 18:22:22 +0000

    Update 102-script

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,2 +1,9 @@
 $(document).ready(function () {
-    // add a click event to the translate button
+    // Click event to the translate button.
+  const url = 'https://www.fourtonfish.com/hellosalut/hello/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
+    });
+  });
+});
